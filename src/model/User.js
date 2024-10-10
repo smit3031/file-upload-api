d4cb25b512a0185e4be55352fd0af88ed5ebfe46
Smit <smitc177@gmail.com>
2024-10-10 19:29:09 +0530

    changes

--- src/model/User.js
+++ src/model/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
+  //compund index
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
 });
